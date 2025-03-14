e0817d4ca874db5c9bec4087dcba42be0a0ecb45
princewillchidi <127307527+princewillchidi@users.noreply.github.com>
2025-03-14 23:44:44 +0000

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
             {{-- Start Trending news section --}}
             <div class="flex justify-between items-center w-11/12 mx-auto mt-12">
                 <div>
-                    <h6 class="text-[13px] font-medium text-black sm:text-4xl sm:text-black">Trending News Hello</h6>
+                    <h6 class="text-[13px] font-medium text-black sm:text-4xl sm:text-black">Trending News</h6>
                 </div>
 
                 <livewire:search-box />
@@ -40,88 +40,45 @@
             </div>
 
             <div class="w-11/12 mx-auto relative overflow-hidden sm:w-11/12 sm:mx-auto sm:mt-8 sm:overflow-visible">
-                <div class="mt-12 h-[304px] overflow-hidden grid grid-flow-col auto-cols-[80%] sm:grid sm:grid-cols-4 sm:gap-x-6 sm:items-center sm:overflow-visible">
+                {{-- Trending News Grid --}}
+                <div class="mt-12 h-[304px] overflow-hidden grid grid-flow-col auto-cols-[80%] gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-6 sm:items-center sm:overflow-visible">
                     @foreach ($ten as $tens)
-                        <div class="w-[267px] h-inherit sm:w-80 sm:h-80">
-                            <a  href="">
-                                <div class="w-[267px] h-[256px] sm:h-full sm:w-full">
+                        <div class="w-[267px] h-inherit sm:w-full sm:h-80">
+                            <a href="{{ route('read.post', $tens->id) }}">
+                                <div class="w-full h-[256px] sm:h-full">
                                     @if ($tens->image)
-                                        <img class="block w-full h-full object-cover rounded-[1rem] sm:h-full sm:w-full sm:rounded-[md]" src="{{$tens->image}}" alt="">
+                                        <img class="w-full h-full object-cover rounded-lg sm:rounded-md" src="{{ $tens->image }}" alt="{{ $tens->title }}">
                                     @elseif ($tens->video)
-                                        <video class="h-full w-full rounded-[1rem] object-cover sm:rounded-md" autoplay muted loop>
-                                            <source src="{{$tens->video}}" type="video/mp4">
+                                        <video class="w-full h-full rounded-lg object-cover sm:rounded-md" autoplay muted loop>
+                                            <source src="{{ $tens->video }}" type="video/mp4">
                                             Your browser does not support the video tag.
                                         </video>
-                                    @else
-                                        {{-- <p>No media available</p> --}}
                                     @endif
                                 </div>
                             </a>
                             <div class="mt-2">
-                                <a class="font-medium text-[12px] hover:text-sky-400 hover:text-[14px] sm:font-medium sm:text-lg sm:hover:text-sky-400 sm:hover:text-xl" href="{{route('read.post', $tens->id)}}">
-                                    {{$tens->title}}
+                                <a class="font-medium text-[12px] hover:text-sky-400 sm:text-lg" href="{{ route('read.post', $tens->id) }}">
+                                    {{ $tens->title }}
                                 </a>
                             </div>
-                            <div class="w-[41px] h-[16px] ml-[155px] -mt-[259px] sm:w-24 sm:-mt-[335px] sm:ml-48">
-                                @if($tens->category)
-                                    <a class="text-[12px] font-medium rounded-full px-14 py-3 bg-white sm:rounded-full sm:text-[15px] sm:font-medium sm:px-6 sm:py-1" href="">{{$tens->category->title}}</a>
-                                @else
-                                    {{-- <a class="rounded-full bg-white font-medium px-6 py-1" href="#">noCategory</a> --}}
-                                @endif
-                            </div>
+                            @if ($tens->category)
+                                <div class="w-[41px] h-[16px] ml-[155px] -mt-[259px] sm:w-24 sm:-mt-[335px] sm:ml-48">
+                                    <a class="text-[12px] font-medium rounded-full px-14 py-3 bg-white sm:rounded-full sm:text-[15px] sm:px-6 sm:py-1" href="">{{ $tens->category->title }}</a>
+                                </div>
+                            @endif
                         </div>
                     @endforeach
                 </div>
 
+                {{-- Navigation Buttons for Mobile --}}
                 <div class="absolute inset-0 flex items-center justify-between p-6 sm:hidden">
-                    <button id="leftButton" class="w-12 h-12 p-1 text-[16px] rounded-full bg-black text-white"><i class='bx bxs-chevron-left' ></i> </button>
-                    <button id="rightButton" class="w-12 h-12 p-1 text-[16px] rounded-full bg-black text-white"><i class='bx bxs-chevron-right' ></i></button>
+                    <button id="leftButton" class="w-12 h-12 p-1 text-[16px] rounded-full bg-black text-white">
+                        <i class='bx bxs-chevron-left'></i>
+                    </button>
+                    <button id="rightButton" class="w-12 h-12 p-1 text-[16px] rounded-full bg-black text-white">
+                        <i class='bx bxs-chevron-right'></i>
+                    </button>
                 </div>
-                {{-- <div class="w-80 h-80">
-                    <a  href="">
-                        <div class="h-full w-full">
-                            <img class="h-full rounded-md " src="{{asset('images/bench-accounting-nvzvOPQW0gc-unsplash.jpg')}}" alt="">
-                        </div>
-                    </a>
-                    <div class="mt-2">
-                        <a class="font-medium text-lg" href="">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        </a>
-                    </div>
-                    <div class="w-64.5 rounded-full flex items-end justify-end">
-                        <a class="rounded-full bg-white font-medium mr-3 absolute top-72 mt-4 px-6 py-1" href="">Sports</a>
-                    </div>
-                </div>
-                <div class="w-80 h-80">
-                    <a  href="">
-                        <div class="h-full w-full">
-                            <img class="h-full rounded-md" src="{{asset('images/cesar-rincon-XHVpWcr5grQ-unsplash.jpg')}}" alt="">
-                        </div>
-                    </a>
-                    <div class="mt-2">
-                        <a class="font-medium text-lg" href="">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        </a>
-                    </div>
-                    <div class="w-64.5 rounded-full flex items-end justify-end">
-                        <a class="rounded-full bg-white font-medium mr-3 absolute top-72 mt-4 px-6 py-1" href="">Society</a>
-                    </div>
-                </div>
-                <div class="w-80 h-80">
-                    <a  href="">
-                        <div class="h-full w-full">
-                            <img class="h-full rounded-md" src="{{asset('images/cesar-rincon-XHVpWcr5grQ-unsplash.jpg')}}" alt="">
-                        </div>
-                    </a>
-                    <div class="mt-2">
-                        <a class="font-medium text-lg" href="">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        </a>
-                    </div>
-                    <div class="w-64.5 rounded-full flex items-end justify-end">
-                        <a class="rounded-full bg-white font-medium mr-3 absolute top-72 mt-4 px-6 py-1" href="">Education</a>
-                    </div>
-                </div> --}}
             </div>
             {{--End Trending news section --}}
 
