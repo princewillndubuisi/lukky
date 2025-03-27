484e8198af89eea69cd57452bcb84931ea251431
princewillchidi <127307527+princewillchidi@users.noreply.github.com>
2025-03-27 21:39:20 +0000

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased bg-dark-navy">
+    <body class="font-sans antialiased bg-light-grey">
         
 
         <div class="">
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,34 @@ export default {
         "./resources/**/*.vue",
     ],
 
+    safelist: [
+        // Safelist arbitrary values if needed
+        'bg-[#D3D9D4]',
+        'text-[#212A31]',
+        'border-[#253944]',
+        'bg-[#124E66]',
+        'text-[#748D92]',
+        // Add any other custom colors you use
+    ],
+
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
+                // Moon Phases Color Palette
+                'dark-navy': '#212A31',
+                'deep-teal': '#253944',
+                'ocean-blue': '#124E66',
+                'grey-blue': '#748D92',
+                'light-grey': '#D3D9D4',
+                
+                // Your existing custom color
                 'custom-gray': '#D9D9D9',
-                'dark-navy': '#D3D9D4',
             },
-
             screens: {
-                'xs': '360px', // Add an extra small breakpoint
+                'xs': '360px',
             },
         },
     },
